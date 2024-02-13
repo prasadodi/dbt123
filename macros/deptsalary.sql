73a1e7e96f56dff2f4ecb82a03dea67d7f1b765f
prasad v <prasadodi.dev@gmail.com>
2024-02-13 11:37:01 +0000

    commit

--- macros/deptsalary.sql
+++ macros/deptsalary.sql
@@ -1,8 +1,8 @@
-{% macro deptsalary %}
+{% macro deptsalary() %}
 
 
 
-select d.department_name,sum(e.salary) As SALARY from Prod.employee e inner join Prod.DEPARTMENT d
+select d.department_name,sum(e.salary) As SALARY from dbt_src.employee e inner join dbt_src.DEPARTMENT d
 on e.department_id=d.department_id
 group by 1
 
